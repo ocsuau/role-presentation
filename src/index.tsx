843af76f592658e0697716f6051019de4b5abe9d
Oscar Cerdán Suau <ocsuau@gmail.com>
2022-07-15 21:17:39 +0200

    enabling strict mode just in production

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,18 @@ import './index.css';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
+const strictMode = process.env.NODE_ENV === 'production';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  strictMode
+  && (
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>)
+  || <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
